8176a0b2d9fec1bfc3ea9399f8ec14474a3e0a5f
Carl Beckley <carlbeckley@gmail.com>
2023-02-11 17:08:16 +0000

    added links to navbar and styled

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,15 @@ import logo from "../images/logo.png";
 const NavBar = () => {
   return (
     <div className="navbar">
-      <img className="navbar__logo" src={logo} alt="logo" />
+      <img className="navbar-logo" src={logo} alt="logo" />
+      <ul className="navbar-links">
+        <li className="navbar-links-item">
+          <a href="/">View Properties</a>
+        </li>
+        <li className="navbar-links-item">
+          <a href="/">Add a Property</a>
+        </li>
+      </ul>
     </div>
   );
 };
