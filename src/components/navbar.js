8155f5eba945889862a1c71b4d742971c0280b60
Carl Beckley <carlbeckley@gmail.com>
2023-02-11 15:44:19 +0000

    logo added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "../styles/navbar.css";
+import logo from "../images/logo.png";
 
 const NavBar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <img className="navbar__logo" src={logo} alt="logo" />
+    </div>
+  );
 };
 
 export default NavBar;
