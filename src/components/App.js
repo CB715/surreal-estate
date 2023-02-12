ae0b46c64e2c03cf488268fdfb28f2f0b0ae72ad
Carl Beckley <carlbeckley@gmail.com>
2023-02-12 18:10:01 +0000

    routes added to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "../styles/app.css";
 import NavBar from "./navbar";
+import Properties from "./properties";
+import AddProperty from "./addProperty";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
+        <Routes>
+          <Route path="/" element={Properties} />
+          <Route path="/add-property" element={AddProperty} />
+        </Routes>
       </header>
     </div>
   );
