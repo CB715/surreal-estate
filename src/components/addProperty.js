03794517fdb9539328d9d0f0d6c24975eabd8cfd
Carl Beckley <carlbeckley@gmail.com>
2023-02-12 17:44:21 +0000

    add property component created

---
+++ src/components/addProperty.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddProperty = () => {
+  return (
+    <>
+      <div className="properties">Add Property Page</div>;
+    </>
+  );
+};
+
+export default AddProperty;
