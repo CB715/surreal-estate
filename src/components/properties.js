b04b2bfbe06880e9e30fdd54c44f6cde75c3e92e
Carl Beckley <carlbeckley@gmail.com>
2023-02-12 17:46:28 +0000

    properties component added

---
+++ src/components/properties.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Properties = () => {
+  return (
+    <>
+      <div className="properties">Properties Page</div>;
+    </>
+  );
+};
+
+export default Properties;
