1fd0972e3d82680577dc5d8be46af4ba51336e87
Carl Beckley <carlbeckley@gmail.com>
2023-02-13 16:02:50 +0000

    form with input and button added

--- src/components/addProperty.js
+++ src/components/addProperty.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/add-property.css";
 
 const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: "",
+      city: "Manchester",
+    },
+  };
+  const [fields, setFields] = useState(initialState.fields);
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className="add-property">
-      <div>Add Property Page</div>
+      <div>
+        <form onSubmit={handleAddProperty}>
+          <label htmlFor="title">
+            Title
+            <input
+              id="title"
+              name="title"
+              value={fields.title}
+              onChange={handleFieldChange}
+            />
+            <button type="submit">Add</button>
+          </label>
+        </form>
+      </div>
     </div>
   );
 };
