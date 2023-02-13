df4510d2f85035b72320a6e22fbab573e80c437d
Carl Beckley <carlbeckley@gmail.com>
2023-02-13 17:23:56 +0000

    added additional fields to form

--- src/components/addProperty.js
+++ src/components/addProperty.js
@@ -7,12 +7,16 @@ const AddProperty = () => {
     fields: {
       title: "",
       city: "Manchester",
+      type: "",
+      bedrooms: "",
+      bathrooms: "",
+      price: "",
+      email: "",
     },
   };
   const [fields, setFields] = useState(initialState.fields);
   const handleAddProperty = (event) => {
     event.preventDefault();
-    console.log(fields);
   };
   const handleFieldChange = (event) => {
     setFields({ ...fields, [event.target.name]: event.target.value });
@@ -30,8 +34,81 @@ const AddProperty = () => {
               value={fields.title}
               onChange={handleFieldChange}
             />
-            <button type="submit">Add</button>
           </label>
+
+          <label htmlFor="city">
+            City
+            <select
+              id="city"
+              name="city"
+              value={fields.city}
+              onChange={handleFieldChange}
+            >
+              <option value="Manchester">Manchester</option>
+              <option value="Leeds">Leeds</option>
+              <option value="Sheffield">Sheffield</option>
+              <option value="Liverpool">Liverpool</option>
+            </select>
+          </label>
+
+          <label htmlFor="type">
+            Type of Property
+            <select
+              id="type"
+              name="type"
+              value={fields.type}
+              onChange={handleFieldChange}
+            >
+              <option value="Flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detached">Semi-Detached</option>
+              <option value="Terraced">Terraced</option>
+              <option value="End of Terrace">End of Terrace</option>
+              <option value="Cottage">Cottage</option>
+              <option value="Bungalow">Bungalow</option>
+            </select>
+          </label>
+
+          <label htmlFor="bedrooms">
+            Number of Bedrooms
+            <input
+              id="bedrooms"
+              name="bedrooms"
+              value={fields.bedrooms}
+              onChange={handleFieldChange}
+            />
+          </label>
+
+          <label htmlFor="bathrooms">
+            Number of Bathrooms
+            <input
+              id="bathrooms"
+              name="bathrooms"
+              value={fields.bathrooms}
+              onChange={handleFieldChange}
+            />
+          </label>
+
+          <label htmlFor="price">
+            Price
+            <input
+              id="price"
+              name="price"
+              value={fields.price}
+              onChange={handleFieldChange}
+            />
+          </label>
+
+          <label htmlFor="email">
+            Email Address
+            <input
+              id="email"
+              name="email"
+              value={fields.email}
+              onChange={handleFieldChange}
+            />
+          </label>
+          <button type="submit">Add</button>
         </form>
       </div>
     </div>
