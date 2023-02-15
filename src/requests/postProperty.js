8fe19fe226a2ed633e632a1f1f5882a0d16ac15c
Carl Beckley <carlbeckley@gmail.com>
2023-02-15 17:28:03 +0000

    added success and error message for alert component

--- src/requests/postProperty.js
+++ src/requests/postProperty.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 
-const postProperty = (data) => {
+const postProperty = (data, setAlert) => {
   axios
     .post("http://localhost:4000/api/v1/PropertyListing", data)
     .then((response) => {
       console.log(response.data);
+      setAlert({
+        message: "Property Added!",
+        isSuccess: true,
+      });
     })
     .catch((error) => {
       console.log(error.response.data);
+      setAlert({
+        message: "Server Error, please try again later.",
+        isSuccess: false,
+      });
     });
 };
 
