db0c12234df53c016f2aaf83351b34d5aeef509a
Carl Beckley <carlbeckley@gmail.com>
2023-02-15 14:08:04 +0000

    created function to post data to api

--- src/requests/postProperty.js
+++ src/requests/postProperty.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
-const postProperty = () => async {
+const postProperty = (data) => {
+  axios
+    .post("http://localhost:4000/api/v1/PropertyListing", data)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+    });
+};
 
-}
-
-export default axios;
+export default postProperty;
