fdb8ca805456e86489c2d7143ab59fb09474be1e
Carl Beckley <carlbeckley@gmail.com>
2023-02-20 19:42:24 +0000

    added links for cities

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../styles/sidebar.css";
 
 const Sidebar = () => {
-    return (
-
-    );
+  return (
+    <div className="sidebar">
+      <ul className="sidebar-links">
+        <li className="sidebar-links-item">
+          <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
+        </li>
+        <li className="sidebar-links-item">
+          <Link to={`/?query={"city": "Leeds"}`}>Leeds</Link>
+        </li>
+        <li className="sidebar-links-item">
+          <Link to={`/?query={"city": "Sheffield"}`}>Sheffield</Link>
+        </li>
+        <li className="sidebar-links-item">
+          <Link to={`/?query={"city": "Liverpool"}`}>Liverpool</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
