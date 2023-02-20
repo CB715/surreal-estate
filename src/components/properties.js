a43f33c685d84f87ff6bcb9e41305a026faac1dc
Carl Beckley <carlbeckley@gmail.com>
2023-02-20 15:04:33 +0000

    added useState and useEffect hooks to properties

--- src/components/properties.js
+++ src/components/properties.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Properties = () => {
+  const initialState = {
+    properties: [],
+  };
+  const [properties, setProperties] = useState(initialState.properties);
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <div className="properties">Properties Page</div>;
