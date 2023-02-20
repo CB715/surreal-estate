4da0776751f8f7701467ea1bffc492e8e97aa516
Carl Beckley <carlbeckley@gmail.com>
2023-02-20 15:37:46 +0000

    added async function to get properties from api

--- src/components/properties.js
+++ src/components/properties.js
@@ -1,17 +1,38 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Properties = () => {
   const initialState = {
     properties: [],
+    alert: {
+      message: "",
+    },
   };
   const [properties, setProperties] = useState(initialState.properties);
+  const [alert, setAlert] = useState(initialState.alert);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function getProperties() {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api-docs/PropertyListing"
+        );
+        setProperties(response.data);
+        setAlert({
+          message: "",
+        });
+      } catch (error) {
+        setAlert({
+          message: "Error, please try again later.",
+        });
+      }
+    }
+  }, []);
 
   return (
-    <>
-      <div className="properties">Properties Page</div>;
-    </>
+    <div>
+      <div className="properties">Properties Page</div>
+    </div>
   );
 };
 
